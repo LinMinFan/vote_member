3a44f811f8ac1514b32deaf71c9f979474140c6f
LinMinFan <cvse00566@gmail.com>
2022-06-25 16:04:55 +0800

    [new] 發起人刪除投票主題功能完成

--- api/remove_vote.php
+++ api/remove_vote.php
@@ -7,15 +7,38 @@ $pdo = pdo();
 $subject_id = $_GET['subject'];
 $admin_id = $_SESSION['id'];
 
-//需刪除資料包括主題 選項 紀錄
+//語法SELECT * FROM `vote_member_subjects` WHERE `admin` = '$admin_id' && `id` = '$subject_id'
+$chk_sql = "SELECT COUNT(*) FROM `vote_member_subjects` WHERE `admin` = '$admin_id' && `id` = '$subject_id'";
+$chk_again = $pdo->query($chk_sql)->fetchColumn();
 
-//刪除資料
-//sql語法DELETE FROM `vote_member_subjects` WHERE `id` = subject_id &&
-//$pdo->exec();
+//0表示非發起者且非正常路徑則回投票中心首頁1則刪除
+//vote_member_subjects資料表主題刪除
+//DELETE FROM `vote_member_subjects` WHERE `admin` = '$admin_id' && `id` = '$subject_id'
+//vote_member_options資料表選項刪除
+//DELETE FROM `vote_member_options` WHERE `subject_id` = '$subject_id'
+//vote_member_log資料表投票紀錄刪除
+//DELETE FROM `vote_member_log` WHERE `subject_id` = '$subject_id'
+//vote_member_note資料表選項紀錄刪除
+//DELETE FROM `vote_member_note` WHERE `subject_id` = '$subject_id'
+if ($chk_again > 0) {
+    $dlsubsql = "DELETE FROM `vote_member_subjects` WHERE `admin` = '$admin_id' && `id` = '$subject_id'";
+    $dloptsql = "DELETE FROM `vote_member_options` WHERE `subject_id` = '$subject_id'";
+    $dllogsql = "DELETE FROM `vote_member_log` WHERE `subject_id` = '$subject_id'";
+    $dlnotesql = "DELETE FROM `vote_member_note` WHERE `subject_id` = '$subject_id'";
+    $pdo->exec($dlsubsql);
+    $pdo->exec($dloptsql);
+    $pdo->exec($dllogsql);
+    $pdo->exec($dlnotesql);
+    //回到刪除投票頁
+    //header('../vote_center.php?delete=activ');
+    to('../vote_center.php?delete=activ');
+}else{
+    to('../vote_center.php');
+}
 
 
-//回到刪除投票頁
-//header('../vote_center.php?delete=activ');
-to('../vote_center.php?delete=activ');
+
+
+
 
 ?>
